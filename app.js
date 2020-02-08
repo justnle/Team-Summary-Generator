a5586ae7fb5aa1afc525941efc1c6fd7a4f8613a
Justin <twoplatinumchainz@gmail.com>
2020-02-08 01:20:53 -0800

    removed some console.logs

--- app.js
+++ app.js
@@ -3,8 +3,6 @@
 const appFunctions = require(__dirname + '/lib/appFunctions');
 
 async function init() {
-  console.log('Initializing...');
-  console.log('--------------------');
   console.log('Please build your team.');
   await appFunctions.addManager();
   await appFunctions.addTeamMember();
